70feb1858c34639f2ecfe3612731f3e301f4ef87
Mike C. Fletcher <mcfletch@jazinga.com>
2011-08-24 16:26:47 -0400

    Fix referenced version for that fix

--- zeroconf/__init__.py
+++ zeroconf/__init__.py
@@ -21,7 +21,7 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 """
-"""0.12.2 update - fix name matching being case-sensitive for certain queries"""
+"""0.12.3 update - fix name matching being case-sensitive for certain queries"""
 
 """0.12.1 update - support for certain broken clients (e.g. aastra phones),
          partial support for negotiating host-names (probe operation)."""
